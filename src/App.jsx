06bd5945519ea37652584a6ae183eb9ac5a43912
muaz64 <muazmuhammad21@gmail.com>
2025-09-08 21:24:57 +0600

    3rd com

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { Mail, Linkedin, Github, Menu, X, Briefcase, Star, Phone, MapPin, Download } from 'lucide-react';
 import myDp from "./assets/muaz dp.jpg"
+import foodapp from "./assets/food-app.png"
+import ecommerce from "./assets/ecommerce.png"
+import saas from "./assets/saas.png"
+import webxpro from "./assets/webxpro.png"
+import ott from "./assets/ott.png"
+import tm from "./assets/tm.png"
 
 // Starry Blob Background Component
 const BlobBackground = () => {
@@ -120,37 +126,37 @@ export default function App() {
     { 
       name: "E-commerce Website", 
       description: "Bevy Commerce is a responsive product page UI designed for a luxury skincare brand — Luméra. This concept showcases a seamless and elegant eCommerce experience, crafted to boost engagement, improve usability, and communicate product value with clarity and style.", 
-      image: "/images/ecommerce.jpg", 
+      image: ecommerce, 
       link: "https://www.figma.com/community/file/1501111316484833480/bevy-commerce-demo-project" 
     },
     { 
       name: "Food Order App", 
       description: "Jhotpot is designed to simplify food delivery for users who crave quick and convenient access to their favorite meals. Whether you're in the mood for burgers, pizza, or chicken, this app delivers it all with just a few taps!", 
-      image: "/images/food-app.png", 
+      image: foodapp, 
       link: "https://www.figma.com/community/file/1456671089008909048/jhotpot-app-project" 
     },
     { 
       name: "SaaS Landing Page", 
       description: "SaaS Landing Page UI Design - a sleek, conversion-focused layout built for modern software, tech, and digital product companies.", 
-      image: "/images/saas.jpg", 
+      image: saas, 
       link: "https://www.figma.com/community/file/1497491039637715044/saas-landing-page" 
     },
     { 
       name: "WebXpro Landing Page", 
       description: "A clean and modern landing page UI design tailored for web and software development companies. This design is built to help startups and tech businesses promote their services and convert visitors into clients with strong visual storytelling and structured CTAs.", 
-      image: "/images/webxpro.jpg", 
+      image: webxpro, 
       link: "https://www.figma.com/community/file/1496896087247224480/webxpro-landing-page" 
     },
     { 
       name: " Marvel OTT UI Concept ", 
       description: "This UI/UX concept is a Marvel-themed OTT (Over-The-Top) streaming platform designed to deliver a sleek, cinematic, and user-friendly experience for superhero fans. The layout is clean, highly visual, and responsive, making it ideal for desktop and smart TV screens.", 
-      image: "/images/js-projects.png", 
+      image: ott, 
       link: "https://www.figma.com/community/file/1494752200040659323/ott-project" 
     },
     { 
       name: "TaskBolt-Modern Task Management UI(Web App)", 
       description: "TaskBolt is a sleek and modern task management interface designed and developed to make productivity effortless.This UI combines a clean dark theme, intuitive navigation, and neatly organized task lists with features like search, priority selection, and tag categorization.", 
-      image: "/images/js-projects.png", 
+      image: tm, 
       link: "https://www.figma.com/community/file/1536636654993025741/taskbolt-design" 
     },
   ], 
